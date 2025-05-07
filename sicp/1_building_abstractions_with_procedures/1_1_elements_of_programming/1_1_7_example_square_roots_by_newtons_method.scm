9d88a6930dcce78c57d7c8580cf8e8ce31e8da3a
0 <ghostoftheshell@mailbox.org>
2025-05-07 19:56:11 +0300

    SICP: 1.1.7 examples

---
+++ sicp/1_building_abstractions_with_procedures/1_1_elements_of_programming/1_1_7_example_square_roots_by_newtons_method.scm
@@ -0,0 +1,28 @@
+(define (abs x)
+  (if (< x 0) (- x) x))
+
+(define (square x)
+  (* x x))
+
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define (improve guess x)
+  (average guess (/ x guess)))
+
+(define (good-enough? guess x)
+  (< (abs (- (square guess) x)) 0.001))
+
+(define (sqrt-iter guess x)
+  (if (good-enough? guess x)
+    guess
+    (sqrt-iter (improve guess x) x)))
+
+(define (sqrt x)
+  (sqrt-iter 1.0 x))
+
+
+(display (sqrt 9)) (newline)
+(display (sqrt (+ 100 37))) (newline)
+(display (sqrt (+ (sqrt 2) (sqrt 3)))) (newline)
+(display (square (sqrt 1000))) (newline)
